1018d400623ef5251b84bac0025b9ee47bae1390
lmg5615 <lmg5615@gmail.com>
2025-07-25 01:12:10 +0000

    update main page

--- scripts/start_applications.sh
+++ scripts/start_applications.sh
@@ -2,5 +2,5 @@
 # streamlit run /root/streamlit-project/main.py --server.port 80 > /dev/null 2> /dev/null < /dev/null &
 # python /root/streamlit-project/backend/app.py > /dev/null 2> /dev/null < /dev/null &
 systemctl restart streamlit
-systemctl restart backend 
+systemctl restart backend psql –U user –d trip_advisor –h lab-edu-rds-aurora.cluster-c7i6u2042a1i.ap-northeast-2.rds.amazonaws.com
 exit 0
