7a7ace5daa2dfe4e7217e4f31fecee1d15fb087a
lmg5615 <lmg5615@gmail.com>
2025-07-25 00:45:45 +0000

    update main page

--- scripts/stop_streamlit.sh
+++ scripts/stop_streamlit.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+# APP_Name=streamlit
 APP_PID=$(pgrep streamlit)
 
-if [ -z "$APP_PID" ]; then
+if [ -z $APP_PID ]
+then
   echo "Application is not running"
 else
-  echo "Killing Streamlit process: $APP_PID"
-  kill -9 "$APP_PID"
+  echo "Kill -9 $APP_PID"
+  kill -9 $APP_PID
   sleep 5
 fi
 
